274a785891aaaee97672b354d43de7721dcf7831
유서린 <sprout3082@gmail.com>
2023-04-08 15:10:24 +0900

    Second Project init

---
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/SecondSeminarApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.SecondSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSeminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSeminarApplication.class, args);
+	}
+
+}
